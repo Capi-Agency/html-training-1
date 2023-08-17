92d12d67b4f1dc6785fb3862a949c8ddaf5587f6
Nhân Vượng <52657226+vuonggluon@users.noreply.github.com>
2023-08-17 18:08:37 +0700

    update newsDetail.php

---
+++ newsDetail.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Document</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="assets/style.css" />
+</head>
+
+<body>
+    <?php include 'commons/header.php'; ?>
+    <main>
+        <!-- Body -->
+        <section class="detailLayout">
+            <div class="detailTime">
+                <div class="detailCategory">News</div>
+                <p>/</p>
+                <div class="detailDate">July 1, 2022</div>
+            </div>
+            <h1>A Brief History on the Beginnings of Modern Architecture</h1>
+            <div class="detailThumb">
+                <img src="assets/images/news-rectangle-7.png" alt="thumbnail">
+                <br>
+                <p>Quis autem vel eum iure reprehenderit</p>
+            </div>
+            <div class="detailContent">
+                <h3>Cursus viverra ornare sed porta feugiat libero maecenas fringilla. Tincidunt maecenas est, adipiscing sollicitudin ultricies vestibulum commodo erat scelerisque a.</h3>
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum.</p>
+                <p>Fugiat quo voluptas nulla pariatur At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
+                <div class="detailLibrary">
+                    <img src="assets/images/news-rectangle-9.png" alt="">
+                    <img src="assets/images/news-rectangle-11.png" alt="">
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum pretium quis scelerisque malesuada volutpat. Mauris nisi, amet id sodales ut commodo. Cursus viverra ornare sed porta feugiat libero maecenas fringilla. Tincidunt maecenas est, adipiscing sollicitudin pellentesque quis et eget ut. In ultricies vestibulum commodo erat scelerisque a. Egestas ultrices diam amet tellus. Adipiscing facilisis faucibus morbi eu tincidunt interdum mollis. In in non odio nec consequat nulla pharetra tempor vel. Porttitor purus aliquet id vel nunc, tellus eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum pretium quis scelerisque malesuada volutpat. Mauris nisi, amet id sodales ut commodo. Cursus viverra ornare sed porta feugiat libero maecenas fringilla. Tincidunt maecenas est, adipiscing sollicitudin pellentesque quis et eget ut. In ultricies vestibulum commodo erat scelerisque a. Egestas ultrices diam amet tellus. Adipiscing facilisis faucibus morbi eu tincidunt interdum mollis. In in non odio nec consequat nulla pharetra tempor vel. Porttitor purus aliquet id vel nunc, tellus eget.</p>
+            </div>
+            <!-- <div class="detailTags">
+                <div class="tagLayout">
+                    <img src="assets/images/news-group.svg" alt="tag-icon">
+                    <p>TAG</p>
+                </div>
+                <div class="tagCollection">
+                    <div><a href="lmao">Architecture</a></div>
+                    <div><a href="lmao">Architecture</a></div>
+                    <div><a href="lmao">Architecture</a></div>
+                </div>
+            </div> -->
+        </section>
+    </main>
+    <?php include 'commons/footer.php'; ?>
+</body>
+
+</html>
